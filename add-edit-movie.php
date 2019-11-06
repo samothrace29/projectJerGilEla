491294b5e275473a8327e4607035c83d25250a4b
kimchi <55229423+sukmabols@users.noreply.github.com>
2019-11-06 16:18:28 +0100

    html

--- add-edit-movie.php
+++ add-edit-movie.php
@@ -22,7 +22,7 @@
             <br>
             <label>Movie Release Year:</label>
             <br>
-            <input type="number" name="year" placeholder="year" maxlength = "4">
+            <input type="number" name="year" placeholder="year" maxlength = "04">
             <br>
             <label>Synopsis:</label>
             <br>
@@ -74,10 +74,85 @@
 
 <?php
 
+if (!empty($_POST)) {
+	// Basics validations
+	if (empty($_POST['title'])) {
+		$errors[] = 'Title is mandatory';
+	}
+	if (empty($_POST['year'])) {
+		$errors[] = 'Year of release is mandatory';
+	}
+	if (count($errors) === 0) {
+		// If no errors, insert into DB
+        require_once 'database.php';
+        
+		// Open a connection to the DBMS
+		$connect = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+        $query = "INSERT INTO movies(title, release_year, synopsis) VALUES('" . $_POST['title'] . "', '" . $_POST['year'] . "')";
+		// Send an SQL request to our DB
+		$result_query = mysqli_query($connect, $query);
+		if ($result_query) {
+			echo 'Movie successfully addded !';
+		} else {
+			echo 'Error inserting into the DB';
+		}
+	} else {
+		echo implode('<br>', $errors);
+	}
+}
+
 require_once 'connect.php';
 $connect = mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD);
 $db_found = mysqli_select_db($connect,'projectejg');
 
-
+if (isset($_POST['send-file'])) {
+    
+    if ($_FILES['my_file']['error'] != UPLOAD_ERR_OK) {
+        echo 'Upload Error';
+    } else {
+        echo '<pre>';
+        var_dump($_FILES);
+        echo '</pre>';
+        
+        $extFoundInArray = array_search(
+            $_FILES['my_file']['type'],
+            array(
+                'jpg' => 'image/jpeg',
+                'png' => 'image/png',
+                'gif' => 'image/gif',
+            )
+        );
+        if ($extFoundInArray === false) {
+            echo 'That is not a picture';
+        } else {
+            $shaFile = sha1_file($_FILES['my_file']['tmp_name']);
+            $nbFiles = 0;
+            $fileName = '';
+            do {
+                $fileName = $shaFile . $nbFiles . '.' . $extFoundInArray;
+                $fullPath = 'uploads' . $fileName;
+                $nbFiles++;
+            } while (file_exists($fullPath));
+            $moved = move_uploaded_file($_FILES['my_file']['tmp_name'], $fullPath);
+            if (!$moved) {
+                echo 'Error error';
+            } else
+                echo "File successfully saved <br>";
+            $user = $_SESSION['user'];
+            require_once 'database.php';
+            //connect to the database
+            $connect = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD);
+            $query = "
+        UPDATE users SET picture = '" . $fileName . "' WHERE nickname='$user'
+        ";
+            $db_found = mysqli_select_db($connect, 'moviedb');
+            $result_query = mysqli_query($connect, $query);
+            if ($result_query)
+                echo '<img src="uploads' . $fileName . '">';
+            else
+                echo 'DID NOT WORK';
+        }
+    }
+}
 
 ?>
